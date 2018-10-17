e2b50382c052221309bb8e048b7f08a456027618
Serhiy Yarosh <m7grey@gmail.com>
2018-10-17 19:04:00 -0400

    Solution - Day 10.

---
+++ day10.py
@@ -0,0 +1,11 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    nums = str(bin(n)[2:]).split('0')
+    lens = [len(num) for num in nums]
+    print(max(lens))
