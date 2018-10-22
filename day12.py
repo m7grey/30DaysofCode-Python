1cc16a6311d9635aad0bd872f6f55f5f97f1fa55
Serhiy Yarosh <m7grey@gmail.com>
2018-10-22 18:29:22 -0400

    Solution - Day 12.

---
+++ day12.py
@@ -0,0 +1,41 @@
+class Person:
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+
+    def printPerson(self):
+        print("Name:", self.lastName + ",", self.firstName)
+        print("ID:", self.idNumber)
+
+
+import builtins
+
+
+def print(*args, **kwargs):
+    if "sep" not in kwargs and args[0] == "Grade: ":
+        builtins.print(*args, **kwargs, sep="")
+    else:
+        builtins.print(*args, **kwargs)
+
+
+class Student(Person):
+    def __init__(self, firstName, lastName, idNumber, scores):
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
+
+    def calculate(self):
+        iAvg = sum(self.scores) / len(self.scores)
+        return 'O' if iAvg > 89 else 'E' if iAvg > 79 else 'A' if iAvg > 69 else 'P' if iAvg > 54 else 'D' if iAvg > 39 else 'T'
+
+
+if __name__ == "__main__":
+    line = input().split()
+    firstName = line[0]
+    lastName = line[1]
+    idNum = line[2]
+    numScores = int(input())
+    scores = list(map(int, input().split()))
+    s = Student(firstName, lastName, idNum, scores)
+    s.printPerson()
+    print("Grade: ", s.calculate())
